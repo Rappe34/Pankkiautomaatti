2ba81243dae886325bd60240fbe727ea4400168c
Rappe34 <92690091+Rappe34@users.noreply.github.com>
2025-12-29 17:36:17 +0200

    FINAL VERSION WITH INPUT VALUDATION

--- PankkiAutomaatti.c
+++ PankkiAutomaatti.c
@@ -11,125 +11,60 @@ typedef struct {
 	int balance;
 } AccountData;
 
-AccountData loadAccountData(const char* accountNumber) {
-	AccountData acc = { 0 };
-	char filename[64];
+void runATM(void);
+bool getAuth(char* enteredAccount, char* enteredPIN, AccountData* acc);
+bool loadAccountData(char* accountNumber, AccountData* acc);
+void saveAccountData(AccountData* acc);
+void withdrawMoney(AccountData* acc);
+void checkBalance(AccountData* acc);
+void readInt(int* n);
+void readStr(char* str, size_t size);
+void clearBuffer(void);
 
-	strcpy(filename, accountNumber);
-	strcat(filename, ".acc");
+/*
+main - tulostaa ohjelman käynnistysviestin ja
+kutsuu ohjelman pääfunktion runATM ja
+runATM-funktion palautuessa
+tulostaa poistumisviestin.
 
-    // Try to get account data from file with account number as name
-	FILE* file = fopen(filename, "r");
-	if (!file) {
-        // Account doesn't exist, return empty acc data
-		acc.accountNumber[0] = '\0';
-		return acc;
-	}
+Paluuarvo (int):
+- 0 ohjelman palautuessa onnistuneesti (standardi main-funktion paluuarvo)
+*/
+int main(void) {
+    printf("Tervetuloa käyttämään pankkiautomaattia!\n");
 
-	strcpy(acc.accountNumber, accountNumber);
+    runATM();
 
-    // Read PIN on the first line of the acc file
-	fgets(acc.pin, sizeof(acc.pin), file);
-    acc.pin[strcspn(acc.pin, "\n")] = '\0';
+    printf("Poistutaan...\n");
 
-    // Read balance from the second line
-    char balanceLine[64];
-	fgets(balanceLine, sizeof(balanceLine), file);
-    acc.balance = atoi(balanceLine);
-
-    fclose(file);
-	return acc;
+    return 0;
 }
 
-void saveAccountData(const AccountData* acc) {
-	char filename[64];
-	strcpy(filename, acc->accountNumber);
-	strcat(filename, ".acc");
+/*
+runATM - kysyy käyttäjältä tunnistautumistietoja,
+kutsuu tunnistautumisfunktion getAuth.
+Jos tunnistautuminen onnistuu, tulostaa päävalikon
+ja kysyy käyttäjältä toimintoa.
+Saatuaan syötteen, avaa käyttäjän syöttämän toiminnon.
+Toistaa tätä, kunnes käyttäjä syöttää lopetusvaihtoehdon.
+*/
+void runATM(void) {
+    bool auth;
+    AccountData acc;
 
-	FILE* file = fopen(filename, "w");
-	if (!file) {
-		return; // Error opening acc file
-	}
-
-    // Overwrite new account data to the acc file
-	fprintf(file, "%s\n", acc->pin);
-	fprintf(file, "%d\n", acc->balance);
-
-	fclose(file);
-}
-
-bool getAuth(AccountData* acc) {
     char enteredAccount[64];
     char enteredPIN[64];
 
     printf("Syötä tilinumero: ");
-    scanf("%s", enteredAccount);
+    readStr(enteredAccount, 64);
     printf("Syötä PIN-koodi: ");
-    scanf("%s", enteredPIN);
+    readStr(enteredPIN, 64);
     printf("\n");
 
-    *acc = loadAccountData(enteredAccount);
+    auth = getAuth(enteredAccount, enteredPIN, &acc);
 
-    // Failed to find the acc file for the given account number
-    if (acc->accountNumber[0] == '\0') {
-        printf("Virheellinen tilinumero...\n");
-        return false;
-    }
+    if (!auth) return;
 
-    // Check if entered PIN matches with the account PIN
-    if (strcmp(enteredPIN, acc->pin) == 0) {
-        printf("Tunnistautuminen onnistui!\n");
-        strcpy(acc->accountNumber, enteredAccount);
-        return true; // Return succesful auth
-    }
-
-    // Return failed auth
-    printf("Virheellinen PIN-koodi...\n");
-    return false;
-}
-
-void withdrawMoney(AccountData* acc) {
-    int amount;
-    printf("===== NOSTO =====\n");
-    printf("Tilisi saldo on %d€\n", acc->balance);
-    printf("Voit nostaa rahaa 20€, 40€, 50€ ja ylöspäin 10€ askelein. Suurin mahdollinen kertanosto on 1000€.\n");
-    printf("Syötä nostettava määrä kokonaislukuna: ");
-    scanf("%d", &amount);
-
-    int new_balance = acc->balance - amount;
-
-    // Calculate number of 50€ and 20€ bills to withdraw
-    int bills_50 = amount / 50 - (amount > 50 && amount % 50 % 20);
-    int bills_20 = (amount - bills_50 * 50) / 20;
-
-    // Make sure withdrawal amount can be made up with 50€ and 20€ bills, if not throw error
-    if (amount <= 0 || amount > 1000 || amount != bills_50 * 50 + bills_20 * 20) {
-        printf("Et voi nostaa tätä rahamäärää.\n");
-        return;
-    }
-    // Throw error if account doesn't have enough money
-    if (new_balance < 0) {
-        printf("Tilillä ei ole tarpeeksi rahaa.\n");
-        return;
-    }
-
-    acc->balance = new_balance;
-
-    printf("\n");
-
-    printf("Nostettiin onnistuneesti %d€\n(", amount);
-	if (bills_50 > 0 && bills_20 > 0) printf("%dx50€ seteliä ja %dx20€ seteliä", bills_50, bills_20);
-	else if (bills_20 == 0) printf("%dx50€ seteliä", bills_50);
-	else printf("%dx20€ seteliä", bills_20);
-    printf(")\nUusi saldo: %d€\n", acc->balance);
-}
-
-void checkBalance(const AccountData* acc) {
-    printf("===== TILIN SALDO =====\n");
-    printf("Tilisi saldo on: %d€\n", acc->balance);
-}
-
-void showMenu(AccountData* acc) {
     int choice;
 
     do {
@@ -139,43 +74,232 @@ void showMenu(AccountData* acc) {
         printf("3. Lopeta\n");
 
         printf("Valitse toiminto: ");
-        scanf("%d", &choice);
+        readInt(&choice);
 
         printf("\n");
 
         switch (choice) {
         case 1:
-            withdrawMoney(acc);
+            withdrawMoney(&acc);
             printf("Paina mitä tahansa näppäintä jatkaaksesi...\n");
             getch();
             break;
         case 2:
-            checkBalance(acc);
+            checkBalance(&acc);
             printf("Paina mitä tahansa näppäintä jatkaaksesi...\n");
             getch();
             break;
         case 3:
-            printf("Poistutaan...\n");
             break;
         default:
             printf("Virheellinen valinta.\n");
         }
     } while (choice != 3);
-}
-
-int main(void) {
-    bool auth;
-    AccountData acc;
-
-    printf("Tervetuloa käyttämään pankkiautomaattia!\n");
-
-    auth = getAuth(&acc);
-
-    if (auth) {
-        showMenu(&acc);
-    }
 
     saveAccountData(&acc);
+}
 
-    return 0;
+/*
+getAuth - koittaa ladata käyttäjän syöttämän tilin tiedot
+kutsumalla loadAccountData-funktion.
+Jos tili löytyy, vertaa käyttäjän syöttämää PIN-koodia
+oikeaan PIN-koodiin ja palauttaa tunnistautumisen onnistumisen boolina.
+
+Parametrit:
+- enteredAccount (char*): pointteri käyttäjän syöttämään tilinumeroon
+- enteredPIN (char*): pointteri käyttäjän syöttämään PIN-koodiin
+- acc (AccountData*): pointteri muuttujaan,
+johon tunnistautumistilin tiedot tallennetaan
+
+Paluuarvo (bool):
+- true, jos tunnistautuminen onnistuu
+- false, jos tunnistautuminen epäonnistuu,
+eli tilinumero tai PIN-koodi on virheellinen
+*/
+bool getAuth(char* enteredAccount, char* enteredPIN, AccountData* acc) {
+    if (!loadAccountData(enteredAccount, acc)) {
+        printf("Virheellinen tilinumero...\n");
+        return false;
+    }
+
+    if (strcmp(enteredPIN, acc->pin) == 0) {
+        printf("Oikea PIN syötetty, tunnistautuminen valmis!\n");
+        return true;
+    }
+
+    printf("Virheellinen PIN-koodi...\n");
+    return false;
+}
+
+/*
+loadAccountData - Koittaa avata tilinumeron nimisen acc-tiedoston
+ja lukea sieltä tilin PIN-koodin ja saldon.
+Jos tämä onnistuu, funktio palauttaa tiedot AccountData-structin muodossa.
+Jos tiedostoa ei ole olemassa, palautetaan tyhjä AccountData structi.
+
+Parametrit:
+- accountNumber (char): ladattavan tilin numero
+
+Paluuarvo (bool):
+- true, jos tilitiedosto löytyy
+- false, jos tilitiedostoa ei löydy
+*/
+bool loadAccountData(char* accountNumber, AccountData* acc) {
+	char filename[64];
+
+	strcpy(filename, accountNumber);
+	strcat(filename, ".acc");
+
+	FILE* file = fopen(filename, "r");
+	if (!file) {
+		return false;
+	}
+
+	strcpy(acc->accountNumber, accountNumber);
+	fgets(acc->pin, sizeof(acc->pin), file);
+    acc->pin[strcspn(acc->pin, "\n")] = '\0';
+
+    char balanceLine[64];
+	fgets(balanceLine, sizeof(balanceLine), file);
+    acc->balance = atoi(balanceLine);
+
+    fclose(file);
+	return true;
+}
+
+/*
+saveAccountData - tallentaa tilin tiedot tiedostoon
+Avaa tilinumeron nimisen acc-tiedoston
+ja tallentaa sinne tilin uudet tiedot.
+
+Parametrit:
+- acc (AccountData*): pointteri tallennettavaan tiliin
+*/
+void saveAccountData(AccountData* acc) {
+	char filename[64];
+	strcpy(filename, acc->accountNumber);
+	strcat(filename, ".acc");
+
+	FILE* file = fopen(filename, "w");
+	if (!file) {
+		return;
+	}
+
+	fprintf(file, "%s\n", acc->pin);
+	fprintf(file, "%d\n", acc->balance);
+
+	fclose(file);
+}
+
+/*
+withdrawMoney - suorittaa rahannoston tililtä
+Kysyy käyttäjältä nostettavan summan, tarkistaa
+noston ehdot ja päivittää tilin saldon, jos nosto
+on sallittu.
+
+Parametrit:
+- acc (AccountData*): pointteri tiliin, jolta rahaa nostetaan
+*/
+void withdrawMoney(AccountData* acc) {
+    int amount;
+    printf("===== NOSTO =====\n");
+    printf("Tilisi saldo on %d€\n", acc->balance);
+    printf("Voit nostaa rahaa 20€, 40€, 50€ ja ylöspäin 10€ askelein. Suurin mahdollinen kertanosto on 1000€.\n");
+    printf("Syötä nostettava määrä kokonaislukuna: ");
+    readInt(&amount);
+
+    printf("\n");
+
+    int new_balance = acc->balance - amount;
+
+    int bills_50 = amount / 50 - (amount > 50 && amount % 50 % 20);
+    int bills_20 = (amount - bills_50 * 50) / 20;
+
+    if (amount <= 0 || amount > 1000 || amount != bills_50 * 50 + bills_20 * 20) {
+        printf("Et voi nostaa tätä rahamäärää.\n");
+        return;
+    }
+    
+    if (new_balance < 0) {
+        printf("Tilillä ei ole tarpeeksi rahaa.\n");
+        return;
+    }
+
+    acc->balance = new_balance;
+
+    printf("Nostettiin onnistuneesti %d€\n(", amount);
+	if (bills_50 > 0 && bills_20 > 0) printf("%dx50€ seteliä ja %dx20€ seteliä", bills_50, bills_20);
+	else if (bills_20 == 0) printf("%dx50€ seteliä", bills_50);
+	else printf("%dx20€ seteliä", bills_20);
+    printf(")\nUusi saldo: %d€\n", acc->balance);
+}
+
+/*
+checkBalance - tulostaa käyttäjän tilillä olevan rahamäärän.
+
+Parametrit:
+- acc (AccountData*): pointteri tarkasteltavaan tiliin
+*/
+void checkBalance(AccountData* acc) {
+    printf("===== TILIN SALDO =====\n");
+    printf("Tilisi saldo on: %d€\n", acc->balance);
+}
+
+/*
+readInt - lukee käyttäjän syötteen merkkijonona readStr-funktiolla,
+varmistaa että se on kokonaisluvun muotoinen,
+muuttaa sen kokonaisluvuksi ja tallentaa sen
+syötetyn lukupointerin muuttujaan.
+
+Parametrit:
+- n (int*): pointteri muuttujaan, johon syöte tallennetaan
+*/
+void readInt(int* n) {
+    char input[64];
+    char* end;
+
+    readStr(input, sizeof(input));
+
+    long val = strtol(input, &end, 10);
+
+    if (end == input || (*end != '\n' && *end != '\0')) {
+        return;
+    }
+
+    *n = (int)val;
+
+    return;
+}
+
+/*
+readStr - lukee käyttäjän syötteen merkkijonona,
+tallentaa sen annetun merkkijonopointterin muuttujaan.
+Jos syöte on liian pitkä, funktio kutsuu clearBuffer-funktion,
+joka tyhjentää syötebufferin.
+
+Parametrit:
+- str (char*): pointteri merkkijonoon, johon syöte tallennetaan
+- size (size_t): maksimi syötteen koko fgets-funktiota varten
+*/
+void readStr(char* str, size_t size) {
+    if (!fgets(str, size, stdin)) {
+        return;
+    }
+
+    if (str[strlen(str) - 1] == '\n') {
+        str[strlen(str) - 1] = '\0';
+    } else {
+        clearBuffer();
+    }
+
+    return;
+}
+
+/*
+clearBuffer - tyhjentää input bufferin,
+joten ohjelman toiminnot eivät looppaa
+useita kertoja yhden liian pitkän syötteen takia.
+*/
+void clearBuffer(void) {
+    while (fgetc(stdin) != '\n');
 }
